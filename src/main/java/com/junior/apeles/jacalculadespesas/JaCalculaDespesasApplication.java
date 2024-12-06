c1615cf6a8c80ef1aeb93ebb21b183ec9689dfe2
juniorapeles <elijunior.apeles2005@gmail.com>
2024-12-05 23:03:46 -0300

    feat first commit

---
+++ src/main/java/com/junior/apeles/jacalculadespesas/JaCalculaDespesasApplication.java
@@ -0,0 +1,13 @@
+package com.junior.apeles.jacalculadespesas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaCalculaDespesasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JaCalculaDespesasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/junior/apeles/jacalculadespesas/JaCalculaDespesasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junior.apeles.jacalculadespesas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaCalculaDespesasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
